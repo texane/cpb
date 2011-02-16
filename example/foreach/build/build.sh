8d9a1216de1d148ec9178fe58ef2bf6abe16772f
texane <texane@dell.fbx.proxad.net>
2011-02-16 05:34:08 -0600

    [update] build script

--- example/foreach/build/build.sh
+++ example/foreach/build/build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
+
+# NUMA_CFLAGS=-DCONFIG_USE_NUMA=1
+# NUMA_LDFLAGS=-lnuma
+
+NUMA_CFLAGS=-DCONFIG_USE_NUMA=0
+NUMA_LDFLAGS=
+
 gcc -Wall -O3 -march=native -Wall \
+$NUMA_CFLAGS \
 ../src/main.c \
--lnuma -lpthread
+-lpthread \
+$NUMA_LDFLAGS \
